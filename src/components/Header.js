d38b204f9d716ed491e5eb9532d04b6699c6c644
Addy <aditya.emails13@gmail.com>
2022-06-30 17:35:45 +0530

    Changed root path

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,10 @@ const Header = () => {
       <span className="header">My Shopping Cart</span>
       <ul className="nav">
         <li>
-          <Link to="/Shopping-Cart-ContextApi/">Home Page</Link>
+          <Link to="/">Home Page</Link>
         </li>
         <li>
-          <Link to="/Shopping-Cart-ContextApi/cart">Cart ({cart.length})</Link>
+          <Link to="/cart">Cart ({cart.length})</Link>
         </li>
       </ul>
     </div>
